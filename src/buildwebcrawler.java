8ee42a4e9162c9d1e467c6dca04de37174317c31
martinjrofficial <martinokikiakinola@yahoo.ie>
2018-03-27 00:03:21 +0100

    Change

--- src/buildwebcrawler.java
+++ src/buildwebcrawler.java
@@ -9,71 +9,88 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashSet;
 
-
-
 class webcrawler {
-	//Hashet to Link Urls 
-	private HashSet<String> links = new HashSet<>();
-	private String url;
-	private List<String> tofile =  new ArrayList<String>();
-
-   public webcrawler() {
-     links = new HashSet<String>();
-   }
-
-   public void getPageLinks(String URL) {
-       System.out.println(URL +" url to craw");
-       setUrl(URL);
-	   if (!links.contains(URL)) {
-	   try {
-	   //Get HTML source document for url using jsoup     
-       Document document = Jsoup.connect(URL).get();
-       //Parse the HTML to extract links to other URLs
-       Elements linksOnPage = document.select("a[href]");
-
-       //Print the urls
-       links.add(URL);
-       tofile.add("");
-       tofile.add("Url to crawl: "+URL);
-       tofile.add("");
-       
-       for (Element page : linksOnPage) {
-    	   
-           getPageLinks(page.attr("abs:href"));
-           tofile.add(page.attr("abs:href"));
-           
-           
-           System.out.println(page.attr("abs:href"));
-       }
-	   } catch (IOException e) {
-           System.err.println("For '" + URL + "': " + e.getMessage());
-       }
-       }
-    }
-   public void setUrl(String url) {
-	    this.url=url;
-   }
-   public void printArray() {		
-	   String FNAME = "testing.txt";
-	    try (BufferedWriter bw = new BufferedWriter(new FileWriter(FNAME))) {
-		    for (String line : tofile) {
-			    bw.write(line + "\n");
-		    }
-
-		    bw.close();
-
-	    } catch (IOException e) {
-		    e.printStackTrace();
-	      }
-   } 
-}
-  
-public class buildwebcrawler {
-	public static void main(String[] args) {
-		webcrawler wc = new webcrawler();
-		wc.getPageLinks("https://wiprodigital.com/");
-	
-
-	}
-}
+    
+    private String url;
+    private HashSet<String> links;
+    private List<String> tofile =  new ArrayList<String>();
    
+   
+    public webcrawler() {
+        links = new HashSet<>();
+        }
+    public void geturls(String URL) {
+       
+       try {
+           links.add(URL);
+           tofile.add("");
+           tofile.add("Url to Crawl :"+URL);
+           tofile.add("");
+           tofile.add("All URLS in ["+ URL + "] are ");
+           tofile.add("");
+           
+    Document document = Jsoup.connect(URL).get();
+    Elements linksOnPage = document.select("a[href]");
+    for (Element page : linksOnPage) {
+       
+       tofile.add(page.attr("abs:href"));
+       
+       System.out.println(page.attr("abs:href"));
+       getLinks(page.attr("abs:href"));    
+               
+    }
+   
+    
+} catch (IOException e) {
+    System.err.println("For '" + URL + "': " + e.getMessage());
+}
+    }
+    public void getPageLinks(String URL) {
+       seturl(URL);
+       
+       tofile.add("First Page "+URL);
+       // System.out.println();
+       geturls(URL);
+       
+    }
+    
+    
+    public void getLinks(String page) {
+       System.out.println("urls");
+    
+        
+       
+              geturls(page);
+   
+          
+       
+    
+    }
+public void seturl(String URL) {
+       url=URL;
+}
+public void printarray() {
+
+           final String FNAME = "testing.txt";
+              try ( BufferedWriter bw = 
+                           new BufferedWriter (new FileWriter (FNAME)) ) 
+              {                    
+                     for (String line : tofile) {
+                           bw.write (line + "\n");
+                     }
+                     
+                     bw.close ();
+                     
+              } catch (IOException e) {
+                     e.printStackTrace ();
+              }
+       }
+}
+class buildwebcrawler{
+public static void main(String[] args) {
+       webcrawler cd = new webcrawler();
+    cd.getPageLinks("https://wiprodigital.com/");
+    cd.printarray();
+    
+}
+}
